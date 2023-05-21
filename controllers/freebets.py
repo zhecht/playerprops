201c954a74e940a67900ad142c61d72a9eecb800
zhecht <zhecht@barracuda.com>
2023-05-21 00:57:50 -0400

    t

--- controllers/freebets.py
+++ controllers/freebets.py
@@ -239,7 +239,10 @@ def writeFanduel():
 				prop = "double" if "double" in marketName else "hr"
 				for playerRow in data["attachments"]["markets"][market]["runners"]:
 					player = playerRow["runnerName"].lower().replace("'", "").replace(".", "").replace("-", " ").replace(" jr", "").replace(" ii", "")
-					odds = playerRow["winRunnerOdds"]["americanDisplayOdds"]["americanOdds"]
+					try:
+						odds = playerRow["winRunnerOdds"]["americanDisplayOdds"]["americanOdds"]
+					except:
+						continue
 
 					if player not in lines[game]:
 						lines[game][player] = {}
@@ -248,7 +251,13 @@ def writeFanduel():
 	with open(f"{prefix}static/baseballreference/fanduelLines.json", "w") as fh:
 		json.dump(lines, fh, indent=4)
 
-def devigger(evData, player="", bet365Odds="575/-900", finalOdds=630):
+def devigger(evData, player="", bet365Odds="575/-900", finalOdds=630, dinger=False):
+
+	if dinger:
+		# assuming 2hr/g = 40% FB @ 70% conversion
+		finalOdds = f"1={finalOdds};n=0.28x"
+		# 80% conversion
+		#finalOdds = f"1={finalOdds};n=0.36x"
 
 	outfile = "out"
 	post = ["curl", 'http://crazyninjamike.com/public/sportsbooks/sportsbook_devigger.aspx', "-X", "POST", "-H", 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0', "-H", 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8', "-H",'Accept-Language: en-US,en;q=0.5', "-H",'Accept-Encoding: gzip, deflate', "-H",'Content-Type: application/x-www-form-urlencoded', "-H",'Origin: http://crazyninjamike.com', "-H",'Connection: keep-alive', "-H",'Referer: http://crazyninjamike.com/public/sportsbooks/sportsbook_devigger.aspx', "-H",'Cookie: General=KellyMultiplier=.25&KellyBankRoll=1000&DevigMethodIndex=4&WorstCaseDevigMethod_Multiplicative=True&WorstCaseDevigMethod_Additive=True&WorstCaseDevigMethod_Power=True&WorstCaseDevigMethod_Shin=True&MultiplicativeWeight=0&AdditiveWeight=0&PowerWeight=0&ShinWeight=0&ShowEVColorIndicator=False&ShowDetailedDevigInfo=True&CopyToClipboard_Reddit=False&ShowHedgeDevigMethod=False&UseMultilineTextbox=False; ASP.NET_SessionId=h2cklqfayhnovxkckdykfm4o', "-H",'Upgrade-Insecure-Requests: 1', "-H",'Pragma: no-cache', "-H",'Cache-Control: no-cache', "--data-raw", '__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKMjA1MjMyNjgxMA9kFgICAw9kFhgCJw8PFgIeB1Zpc2libGVoZGQCNQ8PFgIeBFRleHQFgAI8Yj5Xb3JzdC1jYXNlOiAoUG93ZXIpPC9iPjwvYnI%2BTGVnIzEgKDU3NSk7IE1hcmtldCBKdWljZSA9IDQuOCAlOyBMZWcncyBKdWljZSA9IDQuNSAlOyBGYWlyIFZhbHVlID0gKzc4MyAoMTEuMyAlKTwvYnI%2BRmluYWwgT2RkcyAoKzYzMCk7IM6jKE1hcmtldCBKdWljZSkgPSA0LjgxICU7IM6jKExlZydzIEp1aWNlKSA9IDQuNSAlOyBGYWlyIFZhbHVlID0gKzc4MyAoMTEuMyAlKTwvYnI%2BU3VtbWFyeTsgRVYlID0gLTE3LjMgJSAoRkIgPSA3MS4zICUpZGQCNw8PFgIeCEltYWdlVXJsZWRkAjkPDxYCHwEFBTwvYnI%2BZGQCOw8PFgIfAWVkZAI%2FDw8WAh8BZWRkAkEPDxYCHwFlZGQCRQ8PFgIfAWVkZAJHDw8WAh8BZWRkAksPDxYCHwFlZGQCTQ8PFgIfAWVkZAJRDw8WAh8BZWRkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYTBRNDaGVja0JveENvcnJlbGF0aW9uBQ1DaGVja0JveEJvb3N0BRZSYWRpb0J1dHRvbkJvb3N0UHJvZml0BRNSYWRpb0J1dHRvbkJvb3N0QWxsBRNSYWRpb0J1dHRvbkJvb3N0QWxsBRRDaGVja0JveERhaWx5RmFudGFzeQUOQ2hlY2tCb3hSZXdhcmQFJUNoZWNrQm94TGlzdFdvcnN0Q2FzZU1ldGhvZFNldHRpbmdzJDAFJUNoZWNrQm94TGlzdFdvcnN0Q2FzZU1ldGhvZFNldHRpbmdzJDEFJUNoZWNrQm94TGlzdFdvcnN0Q2FzZU1ldGhvZFNldHRpbmdzJDIFJUNoZWNrQm94TGlzdFdvcnN0Q2FzZU1ldGhvZFNldHRpbmdzJDMFJUNoZWNrQm94TGlzdFdvcnN0Q2FzZU1ldGhvZFNldHRpbmdzJDMFJUNoZWNrQm94TGlzdENvcHlUb0NsaXBib2FyZFNldHRpbmdzJDAFJUNoZWNrQm94TGlzdENvcHlUb0NsaXBib2FyZFNldHRpbmdzJDAFGkNoZWNrQm94TGlzdE1pc2NTZXR0aW5ncyQwBRpDaGVja0JveExpc3RNaXNjU2V0dGluZ3MkMQUaQ2hlY2tCb3hMaXN0TWlzY1NldHRpbmdzJDEFEUNoZWNrQm94U2hvd0hlZGdlBRtDaGVja0JveFVzZU11bHRpbGluZVRleHRib3j3YSa0x8tCP%2FIRsSJbwRSmbvip%2FQ%3D%3D&__VIEWSTATEGENERATOR=75A63123&__EVENTVALIDATION=%2FwEdAD0xhk8EdsoMEQ9SrpSOuYvpVduh02rwiY5pxbMa1RNJ5aQWjQCmzgVoWED6ZF3QmBhQwDtP%2FiI7M6rA7bM7sw79dcexLTc65mHP6HSLc%2Bf1LffPdxAlAXM62AauCNlhmmvcpkkrUUk0wKmeRC%2Bo5Y4X6geodp8Olur%2BmlGM1%2FKrX0%2FlhO7FgJVfVmSrfexz2O8O1Hi%2Fs4JOEIbuo8tqstA4FSD8tQvxjLeGTr%2FZjbpMMCoLzpV5VCswEluevhpgd9wk6hQu8IzOUf9SV1fT41DJrES61htWrWjs6qQMwbhFAInbbXKrUyuaH0WERw6pAco%2FDYrJ17Id28pC779glSqiyuRmS0vMxthemNSZxFYiWcucIPcus%2FOzSDEyoeofXW3aOzoxxlnSXry0La7j6r%2Fs%2BfHYZidJ9iL1XbUgK8hpdJe%2BtzEYMgx%2BTUfzwbS304A%2FY8oqBpGAsYx8Mop1jRjezzZQ%2B9tmmKZqyksYCdJDpzyJeqLpB5t8%2F2GJQ3xLbuvhfOPVw%2FvZ64GRORAnyiUeAIH4rCiLrkEjcbAVN3KRLGcEq1QxEeM%2Fjkzi4zrZhPrdYjFyIOpXU7VVEH8x6qLFK%2FgiX4R7k%2BePj3bS7J%2BgWBTnTMwDkbtKRoDY9CgTs7Im6q5QJGQ4Zop7C9MPh%2FovUZMCC%2B0SDrnxmEkAW9uYtxLt%2Fyzf7D%2FO38iSXXKKjp%2F7pQZSXQqK%2BkHRq0%2F8hxlteb8BqBsXIQ3Id4DTK3MIxXuZIjIbmxHxK2jQvvnkl2IojkGDopgGMUjBgFQSCSRWR%2BvnMJoIOfBUI43VZFoFoOWmJfOGgotEV0SbRtvJlGGgBtNu745bl1XM%2FNjSIkmj0hpqvWk5jUlJo8y5DJZi2FeCfzTe16FW7YBUVAarIwORcYHwHlUbtxUIovxtp6Fnp9PZDrtAO8BRQYb%2BM2XZoW6ZGCGPI3Vs4qn7bWwKvOZxJ0hkpY3JuBY8dUINwhWy9tBMMMleqCemET2u7gRHPAbTtEEA0M6r7Zrzlm%2BXOIAyERXQ8XPFFqsFxOEZFeuxKH%2FI7Em%2FLOYrJZQEF%2FXmnfJtqlZ119DKEMgOAXJKwTjbHfIVMvqWcHvFBXtSrRli%2BLXSuvmx4sYD3Q%2FsmNkE8GK1s%2FT%2FRpPmdWeEsxFtygifU2vOaYXIhnkpGZxXMYRub%2FxoObcM1wBoVb5Wzqjq1UUGVOuNjuWHehORBMX7COFSeqYehFSmr8SovWm91KahWi5xvQwY6WQfKYDVBNSRbUN8dPza19tfAj7Bgb95LWIXvHnlu623MCphxMmD59a%2F8FmjcA2Psl5LiAJUgZxsX0wLL3s%3D&TextBoxKellyMultiplier=.25&TextBoxBankRoll=1000&RadioButtonListDevigMethod=worstcase&TextBoxLegOdds='+str(bet365Odds)+'&TextBoxFinalOdds='+str(finalOdds)+'&TextBoxCorrelation=0&TextBoxBoost=0%25&Boost=RadioButtonBoostProfit&DropDownListDailyFantasy=0&ButtonCalculate=Calculate&CheckBoxListWorstCaseMethodSettings%240=The+Multiplicative%2FNormalization%2FTraditional+Method&CheckBoxListWorstCaseMethodSettings%241=The+Additive+Method&CheckBoxListWorstCaseMethodSettings%242=The+Power+Method&CheckBoxListWorstCaseMethodSettings%243=The+Shin+Method&TextBoxMultiplicativeWeight=0%25&TextBoxAdditiveWeight=0%25&TextBoxPowerWeight=0%25&TextBoxShinWeight=0%25&CheckBoxListMiscSettings%241=Show+Detailed+Devig+Info', "-o", outfile]
@@ -280,7 +289,7 @@ def write365():
 			if (div.innerText == "Player Home Runs") {
 				for (playerDiv of div.parentNode.nextSibling.getElementsByClassName("srb-ParticipantLabelWithTeam")) {
 					let player = playerDiv.getElementsByClassName("srb-ParticipantLabelWithTeam_Name")[0].innerText.toLowerCase().replaceAll(".", "").replaceAll("'", "").replaceAll("-", " ").replaceAll(" jr", "").replaceAll(" ii", "");
-					let team = playerDiv.getElementsByClassName("srb-ParticipantLabelWithTeam_Team")[0].innerText.toLowerCase();
+					let team = playerDiv.getElementsByClassName("srb-ParticipantLabelWithTeam_Team")[0].innerText.toLowerCase().split(" - ")[0];
 
 					if (team === "la angels") {
 						team = "laa";
@@ -330,7 +339,7 @@ def write365():
 	"""
 	pass
 
-def writeEV():
+def writeEV(dinger=False):
 
 	date = str(datetime.now())[:10]
 
@@ -347,7 +356,7 @@ def writeEV():
 	with open(f"{prefix}static/mlbprops/ev.json") as fh:
 		evData = json.load(fh)
 
-	#evData = {}
+	evData = {}
 
 	for game in fdLines:
 		for player in fdLines[game]:
@@ -369,18 +378,27 @@ def writeEV():
 
 			sharpUnderdog = int(bet365Lines[team][player].split("/")[0])
 
+			line = fdLine
+			fd = True
 			if dkLine > fdLine and dkLine > sharpUnderdog:
-				print(fdLine, dkLine, sharpUnderdog, player)
+				line = dkLine
+				fd = False
+				#print(fdLine, dkLine, sharpUnderdog, player)
 
 			if player in evData:
 				continue
-			if fdLine > sharpUnderdog:
+			if line > sharpUnderdog:
 				pass
-				devigger(evData, player, bet365Lines[team][player], fdLine)
+				devigger(evData, player, bet365Lines[team][player], line, dinger)
 				evData[player]["game"] = game
 				evData[player]["team"] = team
 				evData[player]["bet365"] = bet365Lines[team][player]
+				if not fd:
+					fdLine = 0
+					evData[player]["other"] = line
+					evData[player]["otherBook"] = "DK"
 				evData[player]["fanduel"] = fdLine
+
 
 
 	with open(f"{prefix}static/mlbprops/ev.json", "w") as fh:
@@ -398,7 +416,10 @@ def sortEV():
 
 	for row in sorted(data, reverse=True):
 		playerData = row[-1]
-		print(f"{playerData['ev']}% EV: {playerData.get('team', '').upper()} {row[1].title()} +{playerData['fanduel']} FD compared to {playerData['bet365']}. Implied Prob = {playerData['implied'].replace(' ', '')}")
+		line = f"{playerData['fanduel']} FD"
+		if not playerData["fanduel"]:
+			line = f"{playerData['other']} {playerData['otherBook']}"
+		print(f"{playerData['ev']}% EV: {playerData.get('team', '').upper()} {row[1].title()} +{line} compared to {playerData['bet365']}. Implied Prob = {playerData['implied'].replace(' ', '')}")
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -406,14 +427,19 @@ if __name__ == '__main__':
 	parser.add_argument("--ev", action="store_true", help="EV")
 	parser.add_argument("--bpp", action="store_true", help="BPP")
 	parser.add_argument("-p", "--print", action="store_true", help="Print")
+	parser.add_argument("--dinger", action="store_true", help="Dinger Tues")
 
 	args = parser.parse_args()
+
+	dinger = False
+	if args.dinger:
+		dinger = True
 
 	if args.fd:
 		writeFanduel()
 
 	if args.ev:
-		writeEV()
+		writeEV(dinger)
 
 	if args.bpp:
 		checkBPP()
@@ -422,6 +448,6 @@ if __name__ == '__main__':
 		sortEV()
 	#write365()
 	#writeActionNetwork()
-	#devigger()
+	#devigger({}, dinger=dinger)
 
 	freeBet = 170
