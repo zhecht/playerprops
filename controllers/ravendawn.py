112b8fdb0dd4499b22e7cc38564e59cfc4b3beec
zhecht7@gmail.com <root@ZacksPC.localdomain>
2024-02-16 13:38:23 -0500

    t

--- controllers/ravendawn.py
+++ controllers/ravendawn.py
@@ -8,8 +8,17 @@ def ravendawn_route():
 		data = json.load(fh)
 	return render_template("ravendawn.html", data=data)
 
-# each pack is 12000
-# 12000 * demand + (distance * 9)
+@ravendawn_blueprint.route('/updateJSON', methods=["POST"])
+def json_route():
+	data = json.loads(request.data)
+	with open("ravendawn.json", "w") as fh:
+		json.dump(data, fh, indent=4)
+	return jsonify(success=1)
 
 if __name__ == "__main__":
 	pass
+
+	with open("ravendawn.json") as fh:
+		data = json.load(fh)
+
+	lands = ["darzuac", "defiance", "margrove", "orca bay", "rivercrest", "riverend", "seabreeze"]
