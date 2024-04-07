ff34574daf0ef88fb298002641d1dbe9974e4684
Zack Hecht <zhecht7@gmail.com>
2024-04-07 17:38:38 -0400

    t

--- controllers/mlb.py
+++ controllers/mlb.py
@@ -1227,19 +1227,6 @@ def writeFanduel():
 	"""
 
 	games = [
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/arizona-diamondbacks-@-atlanta-braves-33170815",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/philadelphia-phillies-@-washington-nationals-33170819",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/toronto-blue-jays-@-new-york-yankees-33170836",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/baltimore-orioles-@-pittsburgh-pirates-33170845",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/new-york-mets-@-cincinnati-reds-33170823",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/oakland-athletics-@-detroit-tigers-33170837",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/chicago-white-sox-@-kansas-city-royals-33170840",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/seattle-mariners-@-milwaukee-brewers-33170846",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/miami-marlins-@-st.-louis-cardinals-33170826",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/los-angeles-dodgers-@-chicago-cubs-33170829",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/tampa-bay-rays-@-colorado-rockies-33170847",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/san-diego-padres-@-san-francisco-giants-33170834",
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/boston-red-sox-@-los-angeles-angels-33170844",
   "https://mi.sportsbook.fanduel.com/baseball/mlb/houston-astros-@-texas-rangers-33170843"
 ]
 
