fcf7703f3b10aa65ee6feb33da5fadcd6430e6c2
Zack Hecht <zhecht7@gmail.com>
2024-04-09 13:41:38 -0400

    t

--- controllers/mlb.py
+++ controllers/mlb.py
@@ -1227,7 +1227,6 @@ def writeFanduel():
 	"""
 
 	games = [
-  "https://mi.sportsbook.fanduel.com/baseball/mlb/detroit-tigers-@-pittsburgh-pirates-33176077",
   "https://mi.sportsbook.fanduel.com/baseball/mlb/baltimore-orioles-@-boston-red-sox-33176072",
   "https://mi.sportsbook.fanduel.com/baseball/mlb/chicago-white-sox-@-cleveland-guardians-33176071",
   "https://mi.sportsbook.fanduel.com/baseball/mlb/milwaukee-brewers-@-cincinnati-reds-33176046",
