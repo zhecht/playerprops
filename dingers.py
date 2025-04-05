8206199ca25ad1394cf0e1ac8aed8039b9700153
Zack Hecht <zhecht7@gmail.com>
2025-04-05 13:24:17 -0400

    test

--- dingers.py
+++ dingers.py
@@ -451,7 +451,7 @@ async def writeBR(date):
 		await article.scroll_into_view()
 		show = await article.query_selector(".crRLLM")
 		if show:
-			await show.mouse_click()
+			await show.click()
 
 	html = await page.get_content()
 	soup = BS(html, "lxml")
