a0ebc5e780af02f5016f7210059eaa4714bfe948
Zack Hecht <zhecht7@gmail.com>
2025-04-17 17:07:37 -0400

    test

--- controllers/mlb.py
+++ controllers/mlb.py
@@ -2304,7 +2304,7 @@ def writeEV(date, propArg="", bookArg="fd", teamArg="", boost=None, overArg=None
 			for handicap, playerHandicap in handicaps:
 				player = handicaps[(handicap, playerHandicap)]
 
-				if prop == "hr" and handicap == "1.5":
+				if prop == "hr" and playerHandicap == "1.5":
 					print("skipping 2+ hr")
 					continue
 
