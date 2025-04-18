8800d4934895d0aaac4d045d79c7e4a58c4f04f1
Zack Hecht <zhecht7@gmail.com>
2025-04-18 03:50:33 -0400

    test

--- dingers.py
+++ dingers.py
@@ -1439,7 +1439,8 @@ def writeStatsPage(date):
 
 	lastAB = 0
 
-	props = [("h+r+rbi", 1.5), ("tb", 1.5), ("hr", 0.5), ("h", 0.5), ("k", 5.5)]
+	props = [("h+r+rbi", 1.5), ("tb", 1.5), ("sb", 0.5), ("hr", 0.5), ("h", 0.5), ("k", 5.5)]
+	props = [("sb", 0.5)]
 
 	for prop, line in props:
 		isPitcher = prop in ["k"]
